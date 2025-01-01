4e955dc8e15b12e6946022637525d62d93549302
Rufael Melese <116755536+Rufaelu@users.noreply.github.com>
2025-01-01 10:28:08 +0300

    seeders for users added

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 Route::get('proceedApplications/{id}', function (){
     return view('admin_varify_form');
 });
+
+
+Route::post('/login',[authenticate::class, 'login']);
